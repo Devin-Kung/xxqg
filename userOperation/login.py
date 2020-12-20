74a256a34d42ef08d51de5fdbb3e7b288fae576f
PRaichu <49670735+PRaichu@users.noreply.github.com>
2020-12-20 21:34:09 +0800

    Update login.py

--- userOperation/login.py
+++ userOperation/login.py
@@ -10,22 +10,7 @@ def login(browser):
     iframe = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div[1]/div/div/div/iframe')
     browser.switch_to.frame(iframe)
     login_QR_box = browser.find_element_by_xpath('/html/body/div/div/div[1]')
-    # login_QR = browser.find_element_by_xpath('/html/body/div/div/div[1]/div/div[1]/div[1]/img')
     browser.execute_script('arguments[0].scrollIntoView();', login_QR_box)
-    # browser.switch_to_default_content()
-    # server_headers = {
-    #     'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8"
-    # }
-    # server_data = {
-    #     'text': "学习强国登录",
-    #     'desp': "请在5分钟内完成登录！ ![logo](" + login_QR.get_attribute('src') + ")"
-    # }
-    # res = requests.post("https://sc.ftqq.com/SCU110175T0ad6cfa81c5ebb14bb3350eed3f7c4235f3f113d42803.send", headers=server_headers, data=server_data)
-    # print('二维码获取成功')
-
-    # 二维码有效时间5分钟
-    # time.sleep(5*60)
-
     for i in range(60):
         if browser.current_url == 'https://pc.xuexi.cn/points/my-points.html':
             print('--> 登录成功')
