fb601c42e2baad6f1c99dd9e2cc96b82ac125345
PRaichu <1319155597@qq.com>
2021-05-31 15:30:27 +0800

    代码优化

--- userOperation/logout.py
+++ userOperation/logout.py
@@ -1,11 +1,10 @@
-import random
-import time
-from selenium import webdriver
+from random import uniform
+from time import sleep
 
 
 def logout(browser):
     browser.get('https://www.xuexi.cn/')
-    time.sleep(round(random.uniform(1, 2), 2))
+    sleep(round(uniform(1, 2), 2))
     logoutBtn = browser.find_element_by_xpath('//*[@id="root"]/div/header/div[2]/div[2]/span/a')
     logoutBtn.click()
 
