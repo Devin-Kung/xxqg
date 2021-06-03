b589d399db401c8b1ec7f82f8155130dad3317bb
PRaichu <1319155597@qq.com>
2021-06-03 23:45:51 +0800

    优化

--- userOperation/logout.py
+++ userOperation/logout.py
@@ -8,8 +8,3 @@ def logout(browser):
     sleep(round(uniform(1, 2), 2))
     logoutBtn = browser.find_element_by_xpath('//*[@id="root"]/div/header/div[2]/div[2]/span/a')
     logoutBtn.click()
-
-
-if __name__ == '__main__':
-    browser = webdriver.Chrome()
-    logout(browser)
