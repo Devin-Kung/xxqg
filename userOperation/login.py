b62226f8cd36c4080adc8eebe49b65dfdba869b7
PRaichu <1319155597@qq.com>
2021-06-06 10:31:19 +0800

    登录

--- userOperation/login.py
+++ userOperation/login.py
@@ -8,7 +8,7 @@ def login(browser):
     :param browser: browser
     :return: bool，表示是否登录成功
     """
-    browser.get('https://pc.xuexi.cn/points/my-points.html')
+    browser.xuexi_get('https://pc.xuexi.cn/points/my-points.html')
     sleep(2.5)
     print('--> 请在5分钟内扫码完成登录')
     browser.implicitly_wait(10)
