300ba571c0a7fd9e48828d3dda31d8d8cb7b4754
PRaichu <1319155597@qq.com>
2021-06-06 10:31:12 +0800

    登出

--- userOperation/logout.py
+++ userOperation/logout.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 from random import uniform
 from time import sleep
+from custom.xuexi_chrome import XuexiChrome
 
 
-def logout(browser):
-    browser.get('https://www.xuexi.cn/')
+def logout(browser: XuexiChrome):
+    browser.xuexi_get('https://www.xuexi.cn/')
     sleep(round(uniform(1, 2), 2))
     logoutBtn = browser.find_element_by_xpath('//*[@id="root"]/div/header/div[2]/div[2]/span/a')
     logoutBtn.click()
