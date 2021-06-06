b93c4a2d7ab02bd2f5ec6533d8bfcb26d540bab3
PRaichu <1319155597@qq.com>
2021-06-06 10:30:29 +0800

    观看视频

--- operation/watch_video.py
+++ operation/watch_video.py
@@ -3,9 +3,10 @@ from json import loads, dumps
 from time import sleep
 from random import randint, uniform
 from rich.progress import Progress
+from custom.xuexi_chrome import XuexiChrome
 
 
-def watch_video(browser):
+def watch_video(browser: XuexiChrome):
     videoPath = 'data/videos.json'
     with open(videoPath, 'r', encoding='utf-8') as f:
         videos = f.read()
@@ -20,12 +21,9 @@ def watch_video(browser):
             break
 
     url = videos[randIndex]['url']
-    browser.get('https://www.xuexi.cn/0809b8b6ab8a81a4f55ce9cbefa16eff/ae60b027cb83715fd0eeb7bb2527e88b.html')
-    sleep(round(uniform(1, 3), 2))
-    browser.get('https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html#t1jk1cdl7l-5')
-    sleep(round(uniform(1, 3), 2))
-    browser.get(url)
-    sleep(round(uniform(3, 6), 2))
+    browser.xuexi_get('https://www.xuexi.cn/0809b8b6ab8a81a4f55ce9cbefa16eff/ae60b027cb83715fd0eeb7bb2527e88b.html')
+    browser.xuexi_get('https://www.xuexi.cn/4426aa87b0b64ac671c96379a3a8bd26/db086044562a57b441c24f2af1c8e101.html#t1jk1cdl7l-5')
+    browser.xuexi_get(url)
     video = browser.find_element_by_xpath('/html/body/div/div/section/div/div/div/div/div[2]/section/div/div/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/video')
     start = browser.find_element_by_xpath(
         '/html/body/div/div/section/div/div/div/div/div[2]/section/div/div/div/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]')
